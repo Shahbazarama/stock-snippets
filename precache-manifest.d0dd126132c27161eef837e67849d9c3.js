01bbdc456007025e2dc1137165b0348dc8803848
Raffi Shahbazian <raffi.p.shahbazian@gmail.com>
2020-01-10 13:30:33 -0700

    Updates

---
+++ precache-manifest.d0dd126132c27161eef837e67849d9c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b78fd95230759fa2306a0ea190be6d1",
+    "url": "/stock-snippets/index.html"
+  },
+  {
+    "revision": "90eb3a21572cd42452ce",
+    "url": "/stock-snippets/static/js/2.fa90cce7.chunk.js"
+  },
+  {
+    "revision": "df598df56557fef126a77d1d708b05db",
+    "url": "/stock-snippets/static/js/2.fa90cce7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0390f852339ffba98248",
+    "url": "/stock-snippets/static/js/main.622c9d9e.chunk.js"
+  },
+  {
+    "revision": "afadc31ca4c7f567af2d",
+    "url": "/stock-snippets/static/js/runtime-main.5d84a1c9.js"
+  }
+]);
